6ab87821249dc61572511c8726572ab5d127b861
Xorgon <norman.reggox@gmail.com>
2018-03-13 21:09:34 +0000

    Implemented full PID system with Motor and Instruments.

--- Modules/PID_Control_Scheme/PID_Control_Scheme.ino
+++ Modules/PID_Control_Scheme/PID_Control_Scheme.ino
@@ -1,3 +1,6 @@
+#include "Instruments.h"
+#include "Motor.h"
+
 double lastTime;
 double Input, Setpoint;
 double errSum, lastErr;
@@ -7,62 +10,64 @@ double vel;
 double accel;
 double timestep;
 
-void setup()
-{
-  Input = 0;
-  Setpoint = 1;
+Instruments instruments;
+Motor motor;
+uint8_t pwmPin = 10;
+uint8_t in1Pin = 9;
+uint8_t in2Pin = 8;
 
-  errSum = 0;
-  lastErr = 0;
-  lastTime = 0;
+float headingTarget = 180;
 
-  pos = 0;
-  vel = 0;
-  accel = 0;
-  timestep = 0;
+void setup() {
+    Serial.begin(9600);
+    motor = Motor(pwmPin, in1Pin, in2Pin);
 
-  SetTunings_PID(30, 10, 10);
-  Serial.begin(4800);
+    instruments = Instruments(true);
+
+    Input = 0;
+    Setpoint = 1;
+
+    errSum = 0;
+    lastErr = 0;
+    lastTime = 0;
+
+    pos = 0;
+    vel = 0;
+    accel = 0;
+    timestep = 0;
+
+    SetTunings_PID(20, 5, 10);
 }
 
-void loop()
-{
-  /*How long since we last calculated*/
-  double now = micros() / 1000000.0;
-  double timestep = (now - lastTime);
-
-  // System
-  accel = Compute_PID(pos, timestep);
-  /*if (accel > 0.1) {
-    accel = 0.1;
-  }*/
-  vel += accel * timestep;
-  pos += vel * timestep;
-  Serial.println(pos);
+void loop() {
+    /*How long since we last calculated*/
+    double now = micros() / 1000000.0;
+    double timestep = (now - lastTime);
+    float heading = instruments.getHeading();
+    double Output = Compute_PID(headingTarget - heading, timestep) / 10000;
+    motor.setFloatSpeed(Output);
+    Serial.println("Output = " + String(Output) + ",\tHeading = " + String(heading));
 }
 
-double Compute_PID(double Input, double timeChange)
-{
+double Compute_PID(double Input, double timeChange) {
 
 
-  /*Compute all the working error variables*/
-  double error = Setpoint - Input;
-  errSum += (error * timeChange);
-  double dErr = (error - lastErr) / timeChange;
+    /*Compute all the working error variables*/
+    double error = Setpoint - Input;
+    errSum += (error * timeChange);
+    double dErr = (error - lastErr) / timeChange;
 
-  /*Compute PID Output*/
-  double Output = kp * error + ki * errSum + kd * dErr;
+    /*Compute PID Output*/
+    double Output = kp * error + ki * errSum + kd * dErr;
 
-  /*Remember some variables for next time*/
-  lastErr = error;
-  lastTime += timeChange;
-
-  return (Output);
+    /*Remember some variables for next time*/
+    lastErr = error;
+    lastTime += timeChange;
+    return (Output);
 }
 
-void SetTunings_PID(double Kp, double Ki, double Kd)
-{
-  kp = Kp;
-  ki = Ki;
-  kd = Kd;
+void SetTunings_PID(double Kp, double Ki, double Kd) {
+    kp = Kp;
+    ki = Ki;
+    kd = Kd;
 }
